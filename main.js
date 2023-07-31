d29dcfacde55b388518d2e8c8a834c4843d40e8d
chermankarthi <chermankarthick@gmail.com>
2023-07-31 22:01:39 +0530

    add

---
+++ main.js
@@ -0,0 +1,18 @@
+//CHECK VOTER ID ELIGIBLE
+
+
+
+let age=prompt("Enter Your Age");
+
+
+if(age>=18){
+	
+	document.write("Eligible for License");
+	
+}
+
+else{
+	
+	document.write("Not eligible for License");
+	
+}
